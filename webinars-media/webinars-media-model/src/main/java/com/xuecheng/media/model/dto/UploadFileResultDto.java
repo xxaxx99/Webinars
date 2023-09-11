95d18d6afc3332ab78d20fcfb71734ca36a175bb
xx <937815432@qq,com>
2023-09-11 18:03:01 +0800

    feat: 上传文件的参数模型类以及上传后的返回模型类

---
+++ webinars-media/webinars-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,14 @@
+package com.xuecheng.media.model.dto;
+
+import com.xuecheng.media.model.po.MediaFiles;
+
+/**
+ * @Classname UploadFileResultDto
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2023/9/10 15:38
+ * @Created by lzh
+ */
+public class UploadFileResultDto extends MediaFiles {
+
+}
